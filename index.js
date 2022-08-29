097d906ac98cc01a48cfd93f6e0a177f45ec31df
Harytan <92979595+Yarnyan@users.noreply.github.com>
2022-08-29 23:02:56 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', function() {
+    new Swiper('.swiper', {
+        pagination: {
+            el: '.swiper-scrollbar',
+            type: 'progressbar'
+        },
+        slidesPerView: 2,
+        loop: true,
+        autoplay: {
+            delay: 2000,
+        },
+    })
+
+AOS.init();
+new WOW().init();
+ }, false);
+
+ const apps = document.querySelectorAll(".tabs__nav-btn");
+const tabsItems = document.querySelectorAll(".tabs__item");
+
+apps.forEach(onTabClick);
+
+function onTabClick(item) {
+    item.addEventListener("click", function () {
+        let arhip = item;
+        let tabId = arhip.getAttribute("data-tab");
+        let arhipTab = document.querySelector(tabId);
+
+        if( ! arhip.classList.contains('active') ) {
+            apps.forEach(function(item) {
+                item.classList.remove('active');
+            });
+
+            tabsItems.forEach(function(item) {
+                item.classList.remove('active');
+            });
+
+
+            arhip.classList.add('active');
+            arhipTab.classList.add('active');
+        }
+    });
+}
+
+document.querySelector('.tabs__nav-btn').click();
