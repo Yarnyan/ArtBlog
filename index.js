85ed6583ee90d672f2e417780ade6dd09ce38dbb
Harytan <92979595+Yarnyan@users.noreply.github.com>
2022-08-30 01:55:17 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
-    new Swiper('.swiper', {
-        pagination: {
-            el: '.swiper-scrollbar',
-            type: 'progressbar'
-        },
-        slidesPerView: 2,
-        loop: true,
-        autoplay: {
-            delay: 2000,
-        },
-    })
+    if(document.documentElement.clientWidth < 500) {
+        console.log("penis")
+        new Swiper('.swiper', {
+            pagination: {
+                el: '.swiper-scrollbar',
+                type: 'progressbar'
+            },
+            slidesPerView: 1,
+            loop: true,
+            autoplay: {
+                delay: 2000,
+            },
+        })
+    } else {
+        new Swiper('.swiper', {
+            pagination: {
+                el: '.swiper-scrollbar',
+                type: 'progressbar'
+            },
+            slidesPerView: 2,
+            loop: true,
+            autoplay: {
+                delay: 2000,
+            },
+        })
+    }
 new WOW().init();
  }, false);
 
- const apps = document.querySelectorAll(".tabs__nav-btn");
+
+const apps = document.querySelectorAll(".tabs__nav-btn");
 const tabsItems = document.querySelectorAll(".tabs__item");
 apps.forEach(onTabClick);
 
